f9a002282b5706c88c0e8c557e1b5252f6e8e46d
Jengamon <bob.hostern@gmail.com>
2020-02-14 01:33:41 -0500

    Update version

--- src/main/java/io/github/jengamon/novation/LaunchpadXExtensionDefinition.java
+++ src/main/java/io/github/jengamon/novation/LaunchpadXExtensionDefinition.java
@@ -29,7 +29,7 @@ public class LaunchpadXExtensionDefinition extends ControllerExtensionDefinition
    @Override
    public String getVersion()
    {
-      return "0.5";
+      return "0.9";
    }
 
    @Override
